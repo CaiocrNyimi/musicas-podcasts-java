f81e7e7a037046d61e9a075c72288653fc5b6b5e
CaiocrNyimi <161722010+CaiocrNyimi@users.noreply.github.com>
2024-08-02 16:56:05 -0300

    Add files via upload

---
+++ principal/Principal.java
@@ -0,0 +1,36 @@
+package br.com.alura.minhasmusicas.principal;
+
+import br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica musica = new Musica();
+        musica.setTitulo("O Tempo Não Para");
+        musica.setArtista("Cazuza");
+
+        for (int i = 0; i < 1000; i++) {
+            musica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            musica.curte();
+        }
+
+        Podcast podcast = new Podcast();
+        podcast.setTitulo("Flow");
+        podcast.setApresentador("Igor");
+        
+        for (int i = 0; i < 5000; i++) {
+            podcast.reproduz();
+        }
+        for (int i = 0; i < 1000; i++) {
+            podcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(podcast);
+        preferidas.inclui(musica);
+    }
+}
